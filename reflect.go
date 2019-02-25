23c685c4615d24df8d9c4337c843299683fa7093
hongfawang <hongfawang@digitalgd.com.cn>
2019-02-25 21:15:42 +0800

    code

--- reflect.go
+++ reflect.go
@@ -25,25 +25,26 @@ func main() {
 	u1 := new(User)
 	u1.Id = "1"
 	u1.Age = "23"
-	fmt.Println(reflect.TypeOf(u1))
-	if reflect.TypeOf(u1).Kind() == reflect.Ptr {
+	fmt.Println(reflect.TypeOf(u1)) //*main.User 获取任意对象的具体类型
+	if reflect.TypeOf(u1).Kind() == reflect.Ptr { //判断是否属于指针类型
 		fmt.Println("type ptr")
 	}
 	u2 := new(User)
 	u2.Name = "sam"
-	ru1 := reflect.ValueOf(u1).Elem()
-	ru2 := reflect.ValueOf(u2).Elem()
-	ru1Type := ru1.Type()
-	for i := 0; i < ru1.NumField(); i ++ {
-		v := ru1.Field(i)
-		t := v.Type()
-		if t.Kind() == reflect.String && v.String() != "" {
-			fmt.Println(ru1Type.Field(i).Name, v.String()) //字段名 字段值
-			ru2.Field(i).SetString(v.String())
+	//fmt.Println(reflect.TypeOf(u1).Field(1).Name) //panic: reflect: Field of non-struct type
+	//reflect.ValueOf函数返回的是一份值的拷贝，所以前提是我们是传入要修改变量的地址。其次需要我们调用Elem方法找到这个指针指向的值。
+	v1 := reflect.ValueOf(u1).Elem()
+	v2 := reflect.ValueOf(u2).Elem()
+	t1 := reflect.TypeOf(*u1)
+	for i := 0; i < v1.NumField(); i ++ {
+		v := v1.Field(i)
+		if v.Type().Kind() == reflect.String && v.String() != "" {
+			fmt.Println(t1.Field(i).Name, v.String()) //字段名 字段值
+			v2.Field(i).SetString(v.String())
 		}
 	}
 	fmt.Println(u2)
 	//args := []reflect.Value{reflect.ValueOf("JOE")}
-	mv := ru1.MethodByName("Hello")
+	mv := v1.MethodByName("Hello")
 	mv.Call(nil)
 }
