588935ff2b9ff714385b258afaba1d968bce8b41
hongfawang <hongfawang@digitalgd.com.cn>
2019-02-25 21:26:29 +0800

    code

--- reflect.go
+++ reflect.go
@@ -17,11 +17,9 @@ func (u User) Hello() {
                 Fine, thank you. And you?
                 I'm fine too.
         `)
-	//fmt.Println("Hello", "My name is", u.Name)
 }
 
 func main() {
-	//u1 := User{"1", "OK", "12"}
 	u1 := new(User)
 	u1.Id = "1"
 	u1.Age = "23"
@@ -32,7 +30,9 @@ func main() {
 	u2 := new(User)
 	u2.Name = "sam"
 	//fmt.Println(reflect.TypeOf(u1).Field(1).Name) //panic: reflect: Field of non-struct type
+
 	//reflect.ValueOf函数返回的是一份值的拷贝，所以前提是我们是传入要修改变量的地址。其次需要我们调用Elem方法找到这个指针指向的值。
+	//(Type).Field(i).Name 可以拿到结构体的字段名，Type要求是结构体类型
 	v1 := reflect.ValueOf(u1).Elem()
 	v2 := reflect.ValueOf(u2).Elem()
 	t1 := reflect.TypeOf(*u1)
